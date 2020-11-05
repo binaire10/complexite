5a70d09a1bd585a5863c8f4d6656ca052e447ccc
Luuvuregis <regis.yoels@gmail.com>
2020-11-05 17:01:24 +0100

    Change name of testGetOneMaximalCliqueFromGraph() to testGetOneMaximalEmptyZoneFromGraph

--- test/fr.univ_amu/MathTest.java
+++ test/fr.univ_amu/MathTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,12 +24,12 @@ public class MathTest {
     @Test
     /*
       If you want to run the test with the assert line,
-      comment the line "Collections.shuffle(verticesClique);" of "getOneMaximalCliqueFromGraph()" function in Math.java
+      comment the line "Collections.shuffle(verticesClique);" of "getOneMaximalEmptyZoneFromGraph()" function in Math.java
      */
-    void testGetOneMaximalCliqueFromGraph() {
+    void testGetOneMaximalEmptyZoneFromGraph() {
         Graph graph = new Graph(7);
         addTestDatasInGraph(graph);
-        Set<Integer> result = Math.getOneMaximalCliqueFromGraph(graph);
+        Set<Integer> result = Math.getOneMaximalEmptyZoneFromGraph(graph);
         Object[] tableResult = result.toArray();
         //assertEquals(Arrays.toString(tableResult), "[0, 3, 5]");
         System.out.println(Arrays.toString(tableResult));
